a068509af72d4577b53a6f0145e05277990dbf8e
blixondi <blixondi@gmail.com>
2023-05-17 13:40:20 +0700

    add create todo fragment & todo list fragment

--- app/src/main/java/com/shem/todoapp160420033/view/MainActivity.kt
+++ app/src/main/java/com/shem/todoapp160420033/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.shem.todoapp160420033
+package com.shem.todoapp160420033.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shem.todoapp160420033.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

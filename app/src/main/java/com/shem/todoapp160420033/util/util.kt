56ce0c34c17572c637c9c4706ecc7daf41c77402
blixondi <blixondi@gmail.com>
2023-06-07 13:56:57 +0700

    update database & add migration

--- app/src/main/java/com/shem/todoapp160420033/util/util.kt
+++ app/src/main/java/com/shem/todoapp160420033/util/util.kt
@@ -2,11 +2,22 @@ package com.shem.todoapp160420033.util
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.shem.todoapp160420033.model.TodoDatabase
 
 val DB_NAME = "tododb"
 
 fun buildDb(context: Context):TodoDatabase{
-    val db = Room.databaseBuilder(context, TodoDatabase::class.java, "tododb").build()
+    val db = Room.databaseBuilder(context, TodoDatabase::class.java, "tododb")
+        .addMigrations(MIGRATION_1_2)
+        .build()
     return db
+}
+
+val MIGRATION_1_2 = object : Migration(1,2){
+    override fun migrate(database: SupportSQLiteDatabase){
+        database.execSQL("ALTER TABLE todo ADD COLUMN priority INTEGER DEFAULT 3 NOT NULL")
+        database.execSQL("INSERT INTO todo(title,notes,priority) VALUES('Study hard','Play harder',3)")
+    }
 }

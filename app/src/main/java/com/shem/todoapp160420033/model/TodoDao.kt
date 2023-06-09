00b4e21ddd9d85e74be93681985b1028ad6b68cd
blixondi <blixondi@gmail.com>
2023-06-09 16:28:29 +0700

    add is_done column on Model, add new finishTask function on DAO

--- app/src/main/java/com/shem/todoapp160420033/model/TodoDao.kt
+++ app/src/main/java/com/shem/todoapp160420033/model/TodoDao.kt
@@ -16,6 +16,9 @@ interface TodoDao {
     @Query("UPDATE todo SET title=:title, notes=:notes, priority=:priority WHERE uuid = :id")
     suspend fun update(title:String, notes: String, priority:Int, id:Int)
 
+    @Query("UPDATE todo SET is_done=1 WHERE uuid = :id")
+    suspend fun finishTask(id:Int)
+
     @Delete
     fun deleteTodo(todo:Todo)
 }
